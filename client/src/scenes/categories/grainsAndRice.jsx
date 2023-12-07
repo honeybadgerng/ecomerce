fa6e51a0fc42c3c8c116aec384cecbc6d80d53ac
Honeybadger <rajimoshood200482@gmail.com>
2023-12-07 09:43:41 +0100

    updtd endpont 2 avoid inconsquential errors(App.js

---
+++ client/src/scenes/categories/grainsAndRice.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Item from "../../components/Item";
+import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../../state";
+
+const GrainsAndRice = () => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("grainsAndRice");
+  const items = useSelector((state) => state.cart.items);
+  const breakPoint = useMediaQuery("(min-width:600px)");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getItems() {
+    const items = await fetch(
+      "https://emile12server.onrender.com/api/items?populate=image",
+      { method: "GET" }
+    );
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const grainsAndRiceItems = items.filter(
+    (item) => item.attributes.category === "grainsAndRice"
+  );
+
+  return (
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">
+        Rice & <b>Grains</b>
+      </Typography>
+      <Tabs
+        textColor="primary"
+        indicatorColor="primary"
+        value={value}
+        onChange={handleChange}
+        centered
+        TabIndicatorProps={{ sx: { display: breakPoint ? "block" : "none" } }}
+        sx={{
+          m: "25px",
+          "& .MuiTabs-flexContainer": {
+            flexWrap: "wrap",
+          },
+        }}
+      >
+        <Tab label="GRAINS & RICE" value="grainsAndRice" />
+      </Tabs>
+      <Box
+        margin="0 auto"
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, 300px)"
+        justifyContent="space-around"
+        rowGap="20px"
+        columnGap="1.33%"
+      >
+        {value === "grainsAndRice" &&
+          grainsAndRiceItems.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default GrainsAndRice;
