95ca1e5c96958783bffda5694965e11d450f11de
Honeybadger <rajimoshood200482@gmail.com>
2024-04-21 00:43:40 +0100

    updated post details page

--- client/src/scenes/postDetails/PostDetails.jsx
+++ client/src/scenes/postDetails/PostDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+// import Tabs from "@mui/material/Tabs";
+// import Tab from "@mui/material/Tab";
 import { useParams } from "react-router-dom";
-import { shades } from "../../theme";
+// import { shades } from "../../theme";
 
 const PostDetails = () => {
   const { postId } = useParams();
@@ -47,24 +47,28 @@ const PostDetails = () => {
           )}
           {/* POST author */}
           <div>Author: {post.attributes.author?.data?.attributes?.Name}</div>
+          {/* POST Excerpt */}
+          <Box mt={2}>
+            <Typography variant="body1">{post.attributes.Excerpt}</Typography>
+          </Box>
           {/* POST CONTENT */}
           <Box mt={2}>
             <Typography variant="body1">{post.attributes.Content}</Typography>
           </Box>
 
           {/* TABS */}
-          <Box m="20px 0">
+          {/* <Box m="20px 0">
             <Tabs value={value} onChange={handleChange}>
               <Tab label="DESCRIPTION" value="description" />
               <Tab label="AUTHOR" value="author" />
             </Tabs>
-          </Box>
+          </Box> */}
 
           {/* TAB CONTENT */}
-          {value === "description" && <div>{post.attributes.Content}</div>}
+          {/* {value === "description" && <div>{post.attributes.Content}</div>}
           {value === "author" && (
             <div>Author: {post.attributes.author?.data?.attributes?.Name}</div>
-          )}
+          )} */}
         </>
       )}
     </Box>
